2e89ecee3408d88cf1753674465b255c432ae924
Athi Rajkumar <arajkumar@opentext.com>
2023-01-13 13:57:03 -0500

    minor cleanup

--- frontend/pages/main.tsx
+++ frontend/pages/main.tsx
@@ -7,10 +7,11 @@ interface MovieProperties {
   id: number;
   poster_path: String;
 }
-const moviePath = "https://image.tmdb.org/t/p/original/";
+const MOVIEPATH = "https://image.tmdb.org/t/p/original/";
 
 export default function Home() {
   const [movieList, setMovieList] = useState<MovieProperties[]>([]);
+
   useEffect(() => {
     const sendGetRequest = async () => {
       try {
@@ -18,19 +19,16 @@ export default function Home() {
           "https://api.themoviedb.org/3/trending/movie/day?api_key=781cd82e7790ae7403010d9f126bcc2c"
         );
         setMovieList(resp.data.results.slice(0, 5));
-        console.log(setMovieList(resp.data.results.slice(0, 5)));
       } catch (err) {
         console.error(err);
       }
     };
     sendGetRequest();
   }, []);
-  const numbers = [1, 2, 3, 4, 5];
   const listItems = movieList.map((movie) => (
     <div key={movie.id}>
-      {movie.title}
-      <img src={moviePath + movie.poster_path}></img>
-      {console.log(moviePath + movie.poster_path)}
+      <h1>{movie.title}</h1>
+      <img alt="Movie Poster" src={MOVIEPATH + movie.poster_path}></img>
     </div>
   ));
 
