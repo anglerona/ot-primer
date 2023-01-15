0d3d90c986d72929ff3edf57e932001b8babd55a
Aryan Patel <aryanp@opentext.com>
2023-01-15 18:22:07 -0500

    added flow chart

--- backend_server/src/main/java/com/otprimer/backendserver/UserInfo/UserController.java
+++ backend_server/src/main/java/com/otprimer/backendserver/UserInfo/UserController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController // This allows for automatic response body
 @RequestMapping(path="/")
@@ -14,13 +18,30 @@ public class UserController{
 
     private final UserRepository userRepository;
 
+
     public UserController(final UserRepository userRepository){
         this.userRepository = userRepository;
     }
+
+
 
     @GetMapping(path="/allUsers")
     public @ResponseBody Iterable<User> getAllUsers() {
         // This returns a JSON or XML with the users
         return this.userRepository.findAll();
+    }
+
+
+    @GetMapping(path="/getMovies")
+    public void getMovies(){
+        String url = "https://api.themoviedb.org/3/trending/movie/day?api_key=781cd82e7790ae7403010d9f126bcc2c";
+        //String url = "https://jsonplaceholder.typicode.com/users/";
+        RestTemplate restTemplate = new RestTemplate();
+
+//        Object[] movies = restTemplate.getForObject(url, Object[].class);
+//        return Arrays.asList(movies);
+
+
+
     }
 }
