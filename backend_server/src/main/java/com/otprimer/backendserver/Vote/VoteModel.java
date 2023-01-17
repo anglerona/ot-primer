15727d485b0da28b1d02afc21e2007b679171740
Aryan Patel <aryanp@opentext.com>
2023-01-17 11:25:27 -0500

    changes to models

--- backend_server/src/main/java/com/otprimer/backendserver/Vote/VoteModel.java
+++ backend_server/src/main/java/com/otprimer/backendserver/Vote/VoteModel.java
@@ -16,4 +16,26 @@ public class VoteModel {
     private Integer like_dislike;
     private Integer user_id;
     private Integer review_id;
+
+    public VoteModel(){}
+    VoteModel(Integer like_dislike, Integer user_id, Integer review_id){
+        this.like_dislike = like_dislike;
+        this.user_id = user_id;
+        this.review_id = review_id;
+    }
+
+    public Integer getUserID(){return this.user_id;}
+    public Integer getId(){
+        return this.id;
+    }
+    public Integer getReviewID(){return this.review_id; }
+    public Integer getLikeDislike(){return this.like_dislike;}
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+    public void setUserID(Integer user_id){this.user_id = user_id;}
+    public void setLikeDislike(Integer like_dislike){this.like_dislike = like_dislike;}
+    public void setReviewID(Integer review_id){this.review_id = review_id;}
+
 }
