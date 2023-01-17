9641cab5690263440d34af60e18d3fde1d6cebc7
Athi Rajkumar <arajkumar@opentext.com>
2023-01-17 12:03:21 -0500

    add component for Movie

---
+++ frontend/components/Movie.tsx
@@ -0,0 +1,17 @@
+interface MovieProperties {
+  movieTitle: String;
+  movieImg: String;
+}
+
+export default function Movie(props: MovieProperties) {
+  const { movieTitle, movieImg } = props;
+  return (
+    <div id="single-movie-container">
+      <a>{movieTitle}</a>
+      <img
+        alt="Movie Poster"
+        src={"https://image.tmdb.org/t/p/original/" + movieImg}
+      ></img>
+    </div>
+  );
+}
