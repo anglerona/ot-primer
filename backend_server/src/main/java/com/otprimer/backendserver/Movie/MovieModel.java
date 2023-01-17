d0562f3cb688df7887f0093d8e6ab5670cac91f8
Aryan Patel <aryanp@opentext.com>
2023-01-17 09:37:39 -0500

    added more classes

--- backend_server/src/main/java/com/otprimer/backendserver/Movie/MovieModel.java
+++ backend_server/src/main/java/com/otprimer/backendserver/Movie/MovieModel.java
@@ -1,4 +1,21 @@
 package com.otprimer.backendserver.Movie;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+
+
+@Entity
+@Table(name="MovieModel", schema = "public" )
 public class MovieModel {
+    private @Id
+    @GeneratedValue
+    Integer id;
+
+    //foreign key
+    private Integer review_id;
+
+
 }
