289c6ae918dd3225e1832f62e7efa0638dde12ee
Athi Rajkumar <arajkumar@opentext.com>
2023-01-18 12:33:53 -0500

    add comment ui

--- frontend/components/AddComment.tsx
+++ frontend/components/AddComment.tsx
@@ -3,6 +3,7 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 
 interface Movies {
   movies: String[];
@@ -23,15 +24,39 @@ export default function AddComment({ movies }: Movies) {
         backgroundColor: "#cfd8dc",
       }}
     >
-      <TextField id="outlined-basic" label="Comment" variant="outlined" />
-      <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={movies}
-        sx={{ width: 200 }}
-        renderInput={(params) => <TextField {...params} label="Select Movie" />}
-      />
-      <Button variant="outlined">Submit Comment</Button>
+      <Grid
+        container
+        sx={{ height: "100%" }}
+        alignItems="center"
+        justifyContent="center"
+        spacing={0}
+      >
+        <Grid item xs={12}>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <TextField
+              sx={{ width: "75%" }}
+              size="medium"
+              id="outlined-basic"
+              label="Comment"
+              variant="outlined"
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={4}>
+          <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={movies}
+            sx={{ width: 200 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Select Movie" />
+            )}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <Button variant="outlined">Submit Comment</Button>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
