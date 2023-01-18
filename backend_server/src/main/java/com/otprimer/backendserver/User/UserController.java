fdfe0d632e6c017e0ea8eace3fc73fba3b7e69af
Aryan Patel <aryanp@opentext.com>
2023-01-18 13:41:39 -0500

    user controller added

--- backend_server/src/main/java/com/otprimer/backendserver/User/UserController.java
+++ backend_server/src/main/java/com/otprimer/backendserver/User/UserController.java
@@ -22,4 +22,35 @@ public class UserController {
     public Iterable<User> getAllUsers(){
         return this.userRepository.findAll();
     }
+
+    @PostMapping("/")
+    public User addUser(@RequestBody User user){
+        User newUser = this.userRepository.save(user);
+        return newUser;
+    }
+
+    @PutMapping("/{id}/change/username")
+    public User changeUsername(@PathVariable("id") Integer id, @RequestBody User user){
+        Optional<User> optionalUser = this.userRepository.findById(id);
+        if(!optionalUser.isPresent()){
+            return null;
+        }
+        User newUsername = optionalUser.get();
+        newUsername.setUserName(user.getUserName());
+        User newUser = this.userRepository.save(newUsername);
+        return newUser;
+    }
+
+
+    @DeleteMapping("/{id}")
+    public User deleteUser(@PathVariable("id") Integer id){
+        Optional<User> optUser = this.userRepository.findById(id);
+        if(!optUser.isPresent()){
+            return null;
+        }
+        User userToDelete = optUser.get();
+        this.userRepository.delete(userToDelete);
+        return userToDelete;
+    }
+
 }
