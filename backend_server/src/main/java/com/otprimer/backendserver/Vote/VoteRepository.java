18c246ce6bc44cd084b12f1035b29ab711ed093a
Aryan Patel <aryanp@opentext.com>
2023-01-18 10:21:40 -0500

    added repos

--- backend_server/src/main/java/com/otprimer/backendserver/Vote/VoteRepository.java
+++ backend_server/src/main/java/com/otprimer/backendserver/Vote/VoteRepository.java
@@ -1,4 +1,9 @@
 package com.otprimer.backendserver.Vote;
 
-public class VoteRepository {
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VoteRepository extends CrudRepository<VoteModel, Integer> {
 }
